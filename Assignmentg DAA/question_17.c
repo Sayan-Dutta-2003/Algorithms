adbf18e4c7a6364b5d136e60c8264213c1dd0429
Sayan Dutta <100032007+Sayan-Dutta-2003@users.noreply.github.com>
2023-04-09 13:38:22 +0530

    Delete Assignmentg DAA directory

--- Assignmentg DAA/question_17.c
+++
@@ -1,7 +0,0 @@
-/*
-Write C/C++ program to implement KMP string matching
-method to find the pattern string in a text string both given by the
-user. Compute the complexity of the method for a text string of
-length N and pattern string of length M, where N>M.
-*/
-
